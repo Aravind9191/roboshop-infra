f0122bbbbdb3786ac3f1a8a6e680b3b4aaf5dc70
Aravind Leela <aravindleela.a@gmail.com>
2023-03-05 15:20:29 +0530

    Playbook

--- ec2/main.tf
+++ ec2/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z103214126L48SQW30RSR"
-  name    = "${var.component}-dev.devopsb71.online"
+  name    = "${var.component}-dev.leeladevops.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
